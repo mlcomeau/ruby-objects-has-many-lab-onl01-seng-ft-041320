9002500e736a832ab42cf8d28619a45749b4e839
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 19:13:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,19 @@
 class Song
-  attr_accessor :name, :artist 
+  attr_accessor :name, :artist
   @@all = []
-   
-  def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
 
-  def self.all 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
     @@all
   end
-  
-def artist_name 
+
+def artist_name
   self.artist.name
 end
 
-  
-end 
 
+end
