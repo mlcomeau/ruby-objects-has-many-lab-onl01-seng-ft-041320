a8eb257ae8a8951160e771732a0def4f6dc49db2
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 19:26:27 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,15 +1,15 @@
-class Author 
+class Author
   attr_accessor :name
   def initialize(name)
-    @name = name 
+    @name = name
   end
 
-  def posts 
+  def posts
     Post.all.select {|post| post.author == self}
   end
 
   def add_post(post)
-    post.author = self 
+    post.author = self
   end
 
   def add_post_by_title(title)
@@ -17,9 +17,8 @@ class Author
     add_post(post)
   end
 
-  def self.post_count 
-    Post.all.length 
-  end 
-  
-end
+  def self.post_count
+    Post.all.length
+  end
 
+end
