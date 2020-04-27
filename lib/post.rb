a43a01e1ac5ca57d1d62ef8c3a4e8e06fabea921
Megan Comeau <mlcomeau99@gmail.com>
2020-04-27 19:29:47 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,15 @@
 class Post
   attr_accessor :title, :author
+  
   @@all = []
+  
   def initialize(title)
     @title = title
     @@all << self
   end
 
   def self.all
-    @@all?
+    @@all
   end
 
   def author_name
